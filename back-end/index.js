c09b12d69fec3136bb72ed5542eff2b314b0f502
Abdullah Al Mamun <abdullahgodboy@gmail.com>
2024-05-15 08:33:16 +0600

    configuration on packege.json

--- back-end/index.js
+++ back-end/index.js
@@ -20,6 +20,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:8001",
       "http://127.0.0.1:8001",
+      "https://mern-chat-app-eta-two.vercel.app",
     ],
     credentials: true, // Make sure to include this option to allow credentials
   })
--- front-end/src/context/socketContext.jsx
+++ front-end/src/context/socketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   // console.log(test);
   useEffect(() => {
     if (userId) {
-      const socket = io("http://localhost:8001", {
+      const socket = io("https://mern-chat-app-eta-two.vercel.app", {
         query: {
           userId: userId?._id,
         },
